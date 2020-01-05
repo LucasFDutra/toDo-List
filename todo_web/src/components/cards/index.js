77f404f11d053b186048db608fa846e78ad1cb77
LucasFDutra <lucasfelipedutra@gamil.com>
2020-01-04 21:15:17 -0300

    deu ruim, vou precisar refatorar o backend

--- todo_web/src/components/cards/index.js
+++ todo_web/src/components/cards/index.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import Task from '../tasks/index';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
 
 const Card = (card) => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(card.data.task);
+  }, []);
+
+  const taskChecked = () => {
+    // setTasks([task[0], !task[1]])
+  }
+
+  const taskText = (_taskText) => {
+    // setTask([_taskText, task[1]])
+  }
+
   return (
     <div className='card'>
       <h1 className='title'>{card.data.title}</h1>
       {
-        card.data.task.map((task, index) => (
-          <Task key={index} data={task}/>
+        tasks.map((task, index) => (
+          <div className='task'>
+            <input className='taskCheck' type='checkbox' onChange={() => taskChecked()} checked={task[1]} />
+            <input className='taskText' type='text' onChange={(event) => taskText(event.target.value)} value={task[0]}/>
+            <button className='moreOneTask'>+</button>
+          </div>
+
         ))
       }
       <label htmlFor='label' className='cardLabel'>{card.data.label}</label>
     </div>
   );
 };
+
 export default Card;
--- todo_web/src/pages/dashboard/index.js
+++ todo_web/src/pages/dashboard/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 import Card from '../../components/cards/index';
 import Menu from '../../components/menu/index';
-import NewCard from '../../components/newCard/index'
 import menuIcon from '../../assets/menu.svg';
 import Logo from '../../assets/logo.svg';
 import './styles.css';
@@ -10,6 +9,7 @@ import './styles.css';
 const Dashboard = ({ match, history }) => {
   const [cards, setCards] = useState([]);
   const [menuState, setMenuState] = useState(false);
+  const [addNewCard, setAddNewCard] = useState(false);
 
   useEffect(() => {
     const { username } = match.params;
@@ -43,7 +43,17 @@ const Dashboard = ({ match, history }) => {
         <button className='refrash' onClick={() => {loadCards(match.params.username, localStorage.getItem('password'))}}>Refresh</button>
         <button className='logout' onClick={pressLogout}>Logout</button>
       </div>
-      <NewCard />
+      <div className='newCard'>
+        <label type='button' className='newCardAdd' htmlFor="newCardAdd" onClick={() => setAddNewCard(!addNewCard)}>Add A Card</label>
+      </div>
+      {
+        addNewCard && (
+          <div className="addNewCard" onKeyPress={() => setAddNewCard(!addNewCard)}>
+            <Card data={{"title":"Card Title", "task":[''], "label":'none'}}/>
+          </div>
+        )
+      }
+
       <div className='dashboard'>
         {
           menuState && (
